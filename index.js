474b0f8405d15b463cc3a2699416918f2a22fc37
JulianCastillo <julianscastillo10@gmail.com>
2024-01-13 14:50:04 -0500

    prueba 4

--- index.js
+++ index.js
@@ -27,20 +27,4 @@ app.listen(process.env.PORT, () => {
   console.log(`servidor corriendo en puerto ${process.env.PORT}`);
 });
 
-//mi aplicacion
 
-app.get("/", (req, res) => {
-  const htmlResponse = `
-  <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link  rel="stylesheet" href="./styles.css"><title>BLOQUEADO</title>
-    </head>
-    <body>
-    <h1>bloqueado</h1>
-    </body>
-  </html>
-  `;
-  res.send(htmlResponse);
-});
