94ea2561eeb3a29fbb0ba596fb5a6a2b90b79d34
JulianCastillo <julianscastillo10@gmail.com>
2024-01-13 13:46:53 -0500

    prueba 3

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const { dbConnection } = require("./database/config");
 require("dotenv").config();
-const cors=require('cors')
+const cors = require("cors");
 
 const app = express();
 
-//base de datos 
-dbConnection()
+//base de datos
+dbConnection();
 
 //cors
-app.use(cors())
+app.use(cors());
 //DIRECTORIO PUBLICO
 
 app.use(express.static("public"));
 
-
 //leer rutas de json
-app.use(express.json())
-
+app.use(express.json());
 
 //RUTAS
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/events', require('./routes/events'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events", require("./routes/events"));
 
 //ESCUCHAR PETICONES
 
@@ -29,5 +27,20 @@ app.listen(process.env.PORT, () => {
   console.log(`servidor corriendo en puerto ${process.env.PORT}`);
 });
 
-//mi aplicacion 
+//mi aplicacion
 
+app.get("/", (req, res) => {
+  const htmlResponse = `
+  <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link  rel="stylesheet" href="./styles.css"><title>BLOQUEADO</title>
+    </head>
+    <body>
+    <h1>bloqueado</h1>
+    </body>
+  </html>
+  `;
+  res.send(htmlResponse);
+});
